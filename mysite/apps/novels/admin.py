806395fb8bf0e25865f5083662ac03cebff2f4c9
Robert <alsival@hotmail.com>
2021-04-09 01:10:19 +0200

    Falta clase Meta y modelAdmin para Novelas

--- mysite/apps/novels/admin.py
+++ mysite/apps/novels/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Novel
 
-admin.site.register(Novel)
+@admin.register(Novel)
+class NovelAdmin(admin.ModelAdmin):
+    list_display    = ['pk', 'title', 'type', 'structure', 'date_updated', 'n_likes', 'n_dislikes', 'n_views']
+    ordering        = ['pk', 'title']
+    readonly_fields = ('date_created', 'date_updated', 'n_likes', 'n_dislikes', 'n_views')
--- mysite/apps/novels/models.py
+++ mysite/apps/novels/models.py
@@ -1,26 +1,32 @@
 from django.db import models
 
 class Novel(models.Model):
-    title = models.SlugField(max_length=180, null=False)
-    alter_title = models.CharField(max_length=180, null=True, blank=True)
-    description = models.TextField(max_length=666, null=True, blank=True)
-    date_emission = models.DateTimeField(null=True, blank=True)
-    cover_path = models.CharField(max_length=255, null=True, blank=True)
+    title         = models.CharField(max_length=180, null=False)
+    alter_title   = models.CharField(max_length=180, null=True, blank=True)
+    description   = models.TextField(max_length=666, null=True, blank=True)
+    date_emission = models.DateField(null=True, blank=True)
+    cover_path    = models.CharField(max_length=255, null=True, blank=True)
 
     TYPE_CHOICES = [
         ('LN', 'Light novel'),
         ('WN', 'Web novel')
     ]
-    type = models.CharField(max_length=2, null=False, choices=TYPE_CHOICES ,default=TYPE_CHOICES[0][0])
-
     STRUCTURE_CHOICES = [
         ('VOL', 'Volumes'),
         ('CHA', 'Chapters')
     ]
-    structure = models.CharField(max_length=3, null=False, choices=STRUCTURE_CHOICES, default=[0][0])
 
+    type         = models.CharField(max_length=2, null=False, choices=TYPE_CHOICES ,default=TYPE_CHOICES[0][0])
+    structure    = models.CharField(max_length=3, null=False, choices=STRUCTURE_CHOICES, default=STRUCTURE_CHOICES[0][0])
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    n_likes = models.IntegerField(editable=False)
-    n_dislikes = models.IntegerField(editable=False)
-    n_views = models.IntegerField(editable=False)
+    n_likes      = models.IntegerField(editable=False, default=0, verbose_name='Likes')
+    n_dislikes   = models.IntegerField(editable=False, default=0, verbose_name='Dislikes')
+    n_views      = models.IntegerField(editable=False, default=0, verbose_name='Views')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Novela'
+        verbose_name_plural = 'Novelas'
