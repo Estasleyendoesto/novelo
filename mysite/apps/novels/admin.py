013e039fc543b29876f3767953fdab94ffe82aa3
Robert <alsival@hotmail.com>
2021-04-10 00:16:36 +0200

    Modelado completado, falta eliminar imagenes tras reemplazo

--- mysite/apps/novels/admin.py
+++ mysite/apps/novels/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Novel
+from .models import Novel, Volume, Chapter, Illustration
 
 @admin.register(Novel)
 class NovelAdmin(admin.ModelAdmin):
@@ -8,7 +8,50 @@ class NovelAdmin(admin.ModelAdmin):
     readonly_fields    = ('creation_date', 'last_update', 'n_likes', 'n_dislikes', 'n_views')
     list_display_links = ['title']
     list_filter        = ('type', 'structure', 'creation_date')
-    ordering           = ['title']
     search_fields      = ['title']
     
     show_full_result_count = True
+
+
+@admin.register(Volume)
+class VolumeAdmin(admin.ModelAdmin):
+    list_display       = ['title', 'number', 'novel_id', 'creation_date', 'last_update', 'n_likes', 'n_dislikes', 'n_views']
+    ordering           = ['title', 'number']
+    readonly_fields    = ('creation_date', 'last_update', 'n_likes', 'n_dislikes', 'n_views')
+    list_display_links = ['title']
+    list_filter        = ('creation_date', 'last_update')
+    search_fields      = ['title', 'number']
+
+    show_full_result_count = True
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display       = ['title', 'number', 'type', 'volume_id', 'novela', 'creation_date', 'n_likes', 'n_dislikes', 'n_views']
+    ordering           = ['title', 'number']
+    readonly_fields    = ('creation_date', 'last_update', 'n_likes', 'n_dislikes', 'n_views')
+    list_display_links = ['title', 'number']
+    list_filter        = ('type', 'creation_date', 'last_update')
+    search_fields      = ['title', 'number']
+
+    empty_value_display    = 'Capítulo'
+    show_full_result_count = True
+
+    # para list_display[4] mostrar nombre de novela
+    def novela(self, obj):
+        return obj.volume_id.novel_id
+
+@admin.register(Illustration)
+class IllustrationAdmin(admin.ModelAdmin):
+    list_display       = ['name', 'picture', 'novela', 'volume_id', 'uploaded_by', 'upload_date']
+    ordering           = ['name']
+    readonly_fields    = ['upload_date']
+    list_display_links = ['name', 'picture']
+    list_filter        = ('volume_id', 'uploaded_by', 'upload_date')
+    search_fields      = ['name', 'novela', 'volume_id', 'uploaded_by']
+
+    show_full_result_count = True
+
+    # para list_display[2] mostrar nombre de novela
+    def novela(self, obj):
+        return obj.volume_id.novel_id
--- mysite/apps/novels/models.py
+++ mysite/apps/novels/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Novel(models.Model):
-    title         = models.CharField(max_length=180, null=False)
+    title         = models.CharField(max_length=180, null=False, blank=False)
     alter_title   = models.CharField(max_length=180, null=True, blank=True)
     description   = models.TextField(max_length=666, null=True, blank=True)
     date_emission = models.DateField(null=True, blank=True)
-    cover_path    = models.CharField(max_length=255, null=True, blank=True)
+    cover_path    = models.SlugField(max_length=255, null=True, blank=True)
 
     TYPE_CHOICES = [
         ('LN', 'Light novel'),
@@ -13,20 +13,91 @@ class Novel(models.Model):
     ]
     STRUCTURE_CHOICES = [
         ('VOL', 'Volumes'),
+        ('ARC', 'Arcs'),
         ('CHA', 'Chapters')
     ]
 
-    type         = models.CharField(max_length=2, null=False, choices=TYPE_CHOICES ,default=TYPE_CHOICES[0][0], verbose_name='Tipo')
-    structure    = models.CharField(max_length=3, null=False, choices=STRUCTURE_CHOICES, default=STRUCTURE_CHOICES[0][0], verbose_name='Estructura')
+    type          = models.CharField(max_length=2, null=False, choices=TYPE_CHOICES ,default=TYPE_CHOICES[0][0], verbose_name='Tipo')
+    structure     = models.CharField(max_length=3, null=False, choices=STRUCTURE_CHOICES, default=STRUCTURE_CHOICES[0][0], verbose_name='Estructura')
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
-    last_update = models.DateTimeField(auto_now=True)
-    n_likes      = models.IntegerField(editable=False, default=0, verbose_name='Likes')
-    n_dislikes   = models.IntegerField(editable=False, default=0, verbose_name='Dislikes')
-    n_views      = models.IntegerField(editable=False, default=0, verbose_name='Vistas')
+    last_update   = models.DateTimeField(auto_now=True)
+    n_likes       = models.IntegerField(editable=False, default=0, verbose_name='Likes')
+    n_dislikes    = models.IntegerField(editable=False, default=0, verbose_name='Dislikes')
+    n_views       = models.IntegerField(editable=False, default=0, verbose_name='Vistas')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Novela'
+        verbose_name        = 'Novela'
         verbose_name_plural = 'Novelas'
+
+
+class Volume(models.Model):
+    title         = models.CharField(max_length=180, null=False, blank=False)
+    number        = models.SmallIntegerField(verbose_name='nº')
+    description   = models.TextField(max_length=666, null=True, blank=True)
+    cover_path    = models.SlugField(max_length=255, null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    last_update   = models.DateTimeField(auto_now=True, verbose_name='Última actualización')
+    novel_id      = models.ForeignKey('Novel', on_delete=models.CASCADE, null=False, blank=False, related_name='volume_novel', verbose_name='Novela')
+    n_likes       = models.IntegerField(editable=False, default=0, verbose_name='Likes')
+    n_dislikes    = models.IntegerField(editable=False, default=0, verbose_name='Dislikes')
+    n_views       = models.IntegerField(editable=False, default=0, verbose_name='Vistas')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name        = 'Volumen'
+        verbose_name_plural = 'Volúmenes'
+
+
+class Chapter(models.Model):
+    title         = models.CharField(max_length=180, null=True, blank=True)
+    number        = models.SmallIntegerField(verbose_name='nº')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    last_update   = models.DateTimeField(auto_now=True)
+
+    TYPE_CHOICES = [
+        ('PRO', 'Prologue'),
+        ('EPI', 'Epilogue'),
+        ('INT', 'Intermediate'),
+        ('NOR', 'Normal'),
+        ('ATW', 'Autor words'),
+        ('SPE', 'Special')
+    ]
+
+    type          = models.CharField(max_length=3, null=True, choices=TYPE_CHOICES, default=TYPE_CHOICES[3][0], verbose_name='Tipo')
+    volume_id     = models.ForeignKey('Volume', on_delete=models.CASCADE, null=False, blank=False, related_name='chapter_volume', verbose_name='Volumen')
+    n_likes       = models.IntegerField(editable=False, default=0, verbose_name='Likes')
+    n_dislikes    = models.IntegerField(editable=False, default=0, verbose_name='Dislikes')
+    n_views       = models.IntegerField(editable=False, default=0, verbose_name='Vistas')
+
+    def __str__(self):
+        return 'Capítulo {0}'.format(self.number)
+
+    class Meta:
+        verbose_name        = 'Capítulo'
+        verbose_name_plural = 'Capítulos'
+
+
+# Subida de ilustraciones a novelas/id_novela/id_volumen/nombre_imagen.jpg*
+# La instancia es el objeto actual y se mueve mediante las Foreign Keys
+def volume_directory_path(instance, filename):
+    return 'novels/{0}/{1}/{2}'.format(instance.volume_id.novel_id.id, instance.volume_id.id, filename)
+
+# Surge la necesidad de eliminar la imagen anterior si es reemplazada (actualmente no lo hace, solucionar)
+class Illustration(models.Model):
+    name        = models.SlugField(max_length=60, null=True, blank=True)
+    picture     = models.ImageField(upload_to=volume_directory_path, max_length=100, null=False)
+    volume_id   = models.ForeignKey('Volume', on_delete=models.CASCADE, null=False, blank=False, related_name='illustration_volume', verbose_name='Volumen')
+    uploaded_by = models.ForeignKey('users.User', on_delete=models.CASCADE, null=False, blank=False, related_name='illustration_user', verbose_name='Usuario')
+    upload_date = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de subida')
+
+    def __str__(self):
+        return self.name if self.name else self.picture
+
+    class Meta:
+        verbose_name        = 'Ilustración'
+        verbose_name_plural = 'Ilustraciones'
